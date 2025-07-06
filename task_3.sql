63501f223a4e31f9f46810a1b931b36ca6fd8a8c
psycho-254 <abdul.code254@gmail.com>
2025-07-06 22:39:59 +0300

    List all tables in DB

---
+++ task_3.sql
@@ -0,0 +1,8 @@
+-- Write a script that list all the tables in alx_book_store in your MySQL server.
+
+-- The database name will be passed as argument of mysql command
+
+USE alx_book_store;
+
+SHOW TABLES;
+
