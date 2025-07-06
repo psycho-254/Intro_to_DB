e2fa0c0cd13160bad090f0c351bf2b13846e1974
psycho-254 <abdul.code254@gmail.com>
2025-07-06 22:16:17 +0300

    Script to add tables to db

---
+++ task_2.sql
@@ -0,0 +1,41 @@
+USE DATABASE alx_book_store;
+
+CREATE TABLE IF NOT EXISTS Books(
+    book_id PRIMARY KEY,
+    title VARCHAR (130),
+    author_id,
+    price DOUBLE,
+    publication_date DATE,
+
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE IF NOT EXISTS Authors(
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Order_Details(
+    orderdetailid PRIMARY KEY,
+    order_id,
+    book_id INT,
+    quantity DOUBLE,
+
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
